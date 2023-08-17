13cf4377b7b419007e405ebd135133694342db13
wangyupo <1106774262@qq.com>
2023-08-17 14:16:11 +0800

    [+]新增布局修改示例页面

--- src/views/aExample/menuNest/nestMenu2.vue
+++ src/views/aExample/menuNest/nestMenu2.vue
@@ -1,28 +1,22 @@
 <template>
   <div>
-    路由菜单2
-    <div>
-      <el-button type="primary" @click="changeLayoutStyle('LayoutClassic')">LayoutClassic</el-button>
-      <el-button @click="changeLayoutStyle('LayoutSideMenu')">LayoutSideMenu</el-button>
-      <el-button @click="changeLayoutStyle('LayoutHeadMenu')">LayoutHeadMenu</el-button>
-      <el-table mb-1 :data="[]" />
-      <el-pagination :total="100" />
-      <el-date-picker v-model="value" type="date" />
+    <p>你可以点击以下按钮切换整体布局：</p>
+    <div class="mt-3">
+      <el-button type="primary" @click="changeLayoutStyle('LayoutClassic')">经典布局</el-button>
+      <el-button @click="changeLayoutStyle('LayoutSideMenu')">菜单标题一体布局</el-button>
+      <el-button @click="changeLayoutStyle('LayoutHeadMenu')">顶部菜单布局</el-button>
     </div>
   </div>
 </template>
 
 <script setup>
 import { useLayoutStore } from "@/stores/layout.js";
-import { ref } from "vue";
 
 const layoutStore = useLayoutStore();
 
 const changeLayoutStyle = type => {
   layoutStore.changeLayoutType(type);
 };
-
-const value = ref();
 </script>
 
 <style lang="scss" scoped></style>
