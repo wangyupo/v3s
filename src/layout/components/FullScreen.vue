2e0e5e6e924fac5707e96456d1f1c7d6250baded
wangyupo <1106774262@qq.com>
2023-08-23 13:50:57 +0800

    [+]add FullScreen component

---
+++ src/layout/components/FullScreen.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- 页面全屏组件 -->
+  <el-tooltip effect="dark" :content="isFullscreen ? '取消全屏' : '全屏'" placement="bottom" :show-after="250">
+    <el-icon :size="20" class="ml-3 cursor-pointer" @click="toggle">
+      <FullScreen />
+    </el-icon>
+  </el-tooltip>
+</template>
+
+<script setup>
+import { useFullscreen } from "@vueuse/core";
+
+const { isFullscreen, enter, exit, toggle } = useFullscreen();
+</script>
+
+<style lang="scss" scoped></style>
--- src/layout/components/HeaderRightTools.vue
+++ src/layout/components/HeaderRightTools.vue
@@ -3,6 +3,8 @@
   <div class="flex items-center justify-center mr-4 text-white">
     <!-- 菜单搜索 -->
     <MenuFilter />
+    <!-- 网页全屏 -->
+    <FullScreen />
     <!-- 多语言切换 -->
     <SwitchLanguage class="ml-4" />
     <!-- 切换主题颜色 -->
@@ -17,6 +19,7 @@ import ThemeColorPicker from "./ThemeColorPicker.vue";
 import Avatar from "./Avatar.vue";
 import SwitchLanguage from "./SwitchLanguage.vue";
 import MenuFilter from "./MenuFilter/Index.vue";
+import FullScreen from "./FullScreen.vue";
 </script>
 
 <style lang="scss" scoped></style>
