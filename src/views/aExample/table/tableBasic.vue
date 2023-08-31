a13574fe675455941de712baee8e97d7232b3d71
wangyupo <1106774262@qq.com>
2023-08-31 10:21:30 +0800

    [^]RhTable empty fit drak mode

--- src/views/aExample/table/tableBasic.vue
+++ src/views/aExample/table/tableBasic.vue
@@ -33,26 +33,26 @@ const tableData = reactive({
     },
   ],
   data: [
-    {
-      date: "2016-05-03",
-      name: "",
-      address: "No. 189, Grove St, Los Angeles",
-    },
-    {
-      date: "2016-05-02",
-      name: "Tom",
-      address: "No. 189, Grove St, Los Angeles",
-    },
-    {
-      date: "2016-05-04",
-      name: "Tom",
-      address: "No. 189, Grove St, Los Angeles",
-    },
-    {
-      date: "2016-05-01",
-      name: "Tom",
-      address: "No. 189, Grove St, Los Angeles",
-    },
+    // {
+    //   date: "2016-05-03",
+    //   name: "",
+    //   address: "No. 189, Grove St, Los Angeles",
+    // },
+    // {
+    //   date: "2016-05-02",
+    //   name: "Tom",
+    //   address: "No. 189, Grove St, Los Angeles",
+    // },
+    // {
+    //   date: "2016-05-04",
+    //   name: "Tom",
+    //   address: "No. 189, Grove St, Los Angeles",
+    // },
+    // {
+    //   date: "2016-05-01",
+    //   name: "Tom",
+    //   address: "No. 189, Grove St, Los Angeles",
+    // },
   ],
   pages: {
     total: 100,
