cd8c3a47f0aed9759417fa68de464ae5542eaa6f
wangyupo <1106774262@qq.com>
2023-11-16 17:46:09 +0800

    [^]调正部分暗色样式

--- src/layout/components/SidebarFolder.vue
+++ src/layout/components/SidebarFolder.vue
@@ -1,6 +1,11 @@
 <template>
   <!-- 布局专用-侧边栏折叠器 -->
-  <el-icon class="text-white cursor-pointer" size="20" @click="toggleMenuFold">
+  <el-icon
+    class="cursor-pointer"
+    :class="[isDark ? 'text-[var(--el-text-color-primary)]' : 'text-white']"
+    size="20"
+    @click="toggleMenuFold"
+  >
     <Fold v-show="!menuFold" />
     <Expand v-show="menuFold" />
   </el-icon>
@@ -9,7 +14,7 @@
 <script setup>
 import { useLayout } from "@/hooks/useLayout.js";
 
-const { menuFold, toggleMenuFold } = useLayout();
+const { menuFold, toggleMenuFold, isDark } = useLayout();
 </script>
 
 <style lang="scss" scoped></style>
