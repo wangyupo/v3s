57a76940e71a50fa65ac1cfb45c3a633b804c8a0
融合应用事业部-王玉珀 <1106774262@qq.com>
2023-11-16 16:11:04 +0800

    [^]调整样式

--- src/components/RhNoData.vue
+++ src/components/RhNoData.vue
@@ -3,7 +3,7 @@
   <div class="rh-noData flex items-center justify-center relative w-full h-full">
     <!-- 这是一个罩子，目的是不让用户选中文字、拖动图片，优化体验 -->
     <div class="absolute top-0 left-0 right-0 bottom-0 z-10"></div>
-    <div class="rh-noData-main relative">
+    <div class="rh-noData-main relative leading-none">
       <el-empty :description="description" :image-size="imageSize" v-if="!$slots.empty" />
     </div>
   </div>
