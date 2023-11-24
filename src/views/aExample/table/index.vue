dc673037daa2e5a08553bbdd6827d79d6510c987
wangyupo <1106774262@qq.com>
2023-11-24 15:44:41 +0800

    [^]优化table示例展现形式

--- src/views/aExample/table/index.vue
+++ src/views/aExample/table/index.vue
@@ -1,23 +1,10 @@
 <template>
   <!-- 示例-table样式 -->
-  <div>
-    <RhTitle divider title="基础样式">
-      <template #title-right>
-        <el-button type="primary">新增</el-button>
-      </template>
-    </RhTitle>
-    <tableBasic class="mt-3" />
-    <RhTitle divider title="搜索样式" class="mt-3" />
-    <tableForm class="mt-3" />
-    <RhTitle divider title="线框样式" class="mt-3" />
-    <tableWrapper class="mt-3" />
-    <RhTitle divider title="行编辑" class="mt-3" />
-    <tableRowEdit class="mt-3" />
-    <RhTitle divider title="列字段配置（排序、显示/隐藏）" class="mt-3" />
-    <tableColumnsDIY class="mt-3" />
-    <RhTitle divider title="行拖拽（必须设置row-key，且保证是唯一不重复的）" class="mt-3" />
-    <tableDragRow class="mt-3" />
-  </div>
+  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+    <el-tab-pane :label="item.label" :name="index" v-for="(item, index) in tabList" :key="index">
+      <component :is="item.component"></component>
+    </el-tab-pane>
+  </el-tabs>
 </template>
 
 <script setup>
@@ -27,6 +14,17 @@ import tableWrapper from "./tableWrapper.vue";
 import tableRowEdit from "./tableRowEdit.vue";
 import tableColumnsDIY from "./tableColumnsDIY.vue";
 import tableDragRow from "./tableDragRow.vue";
+import { ref } from "vue";
+
+const activeName = ref(0);
+const tabList = [
+  { label: "基础样式", component: tableBasic },
+  { label: "搜索样式", component: tableForm },
+  { label: "线框样式", component: tableWrapper },
+  { label: "行编辑", component: tableRowEdit },
+  { label: "列字段配置（排序、显示/隐藏）", component: tableColumnsDIY },
+  { label: "行拖拽（必须设置row-key，且保证是唯一不重复的）", component: tableDragRow },
+];
 </script>
 
 <style lang="scss" scoped></style>
