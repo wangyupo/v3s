9802ed3c8587b57421840cf3fe64483e5278d47f
融合应用事业部-王玉珀 <1106774262@qq.com>
2024-02-02 17:58:50 +0800

    [#]修复横向菜单不出现省略号问题

--- src/layout/components/Menu/Index.vue
+++ src/layout/components/Menu/Index.vue
@@ -10,7 +10,24 @@
     :collapse-transition="false"
     :default-active="$route.meta.targetMenuPath || $route.path"
   >
-    <MenuItem :menu="_menu" />
+    <template v-for="menu in _menu">
+      <template v-if="menu[menuKey.children] && menu[menuKey.children].length > 0">
+        <el-sub-menu :key="menu[menuKey.id]" :index="menu[menuKey.id]">
+          <template #title>
+            <i :class="['iconfont text-xl mr-2 menuItem-ew-icon', menu[menuKey.icon]]" v-if="menu[menuKey.icon]"></i>
+            <span>{{ menu[menuKey.title] }}</span>
+          </template>
+          <MenuItem :menu="menu[menuKey.children]" />
+        </el-sub-menu>
+      </template>
+
+      <el-menu-item v-else-if="menu[menuKey.url]" :index="menu[menuKey.url]" :key="menu[menuKey.id]">
+        <i :class="['iconfont text-xl mr-2 menuItem-ew-icon', menu[menuKey.icon]]" v-if="menu[menuKey.icon]"></i>
+        <template #title>
+          <span>{{ menu[menuKey.title] }}</span>
+        </template>
+      </el-menu-item>
+    </template>
   </el-menu>
 </template>
 
