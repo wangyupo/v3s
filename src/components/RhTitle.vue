a59e5fdc58361c35dbbed70233ce8b9bf5f4892b
wangyupo <1106774262@qq.com>
2024-04-21 15:39:17 +0800

    [+]RhTitle新增media传参，支持媒体查询调整字体、高度

--- src/components/RhTitle.vue
+++ src/components/RhTitle.vue
@@ -2,11 +2,19 @@
   <!-- section标题（形式：左侧是线，右侧是标题。常用于区分不容内容块。） -->
   <div
     class="rh-title flex items-center justify-between text-[var(--el-text-color-primary)]"
-    :class="[divider ? 'pb-3 border-b border-b-[var(--el-border-color-light)]' : '', nomb ? '' : 'mb-4']"
+    :class="[
+      divider ? 'pb-3 border-b border-b-[var(--el-border-color-light)]' : '',
+      nomb ? '' : 'mb-4',
+      media ? 'max-xl:h-[36px] max-xl:text-[12px] xl:h-[46px] xl:text-[16px]' : 'h-[46px] text-[16px]',
+    ]"
   >
     <!-- 标题 -->
     <div class="flex items-center">
-      <span class="rh-title-divider h-4" :style="{ backgroundColor: lineColor }"></span>
+      <span
+        class="rh-title-divider"
+        :class="media ? 'max-xl:h-[14px] xl:h-[16px]' : 'h-[16px]'"
+        :style="{ backgroundColor: lineColor }"
+      ></span>
       <slot name="title">
         {{ title }}
       </slot>
@@ -30,6 +38,12 @@ const props = defineProps({
     type: String,
     default: "var(--el-color-primary)",
   },
+  // 是否媒体响应（通常用在满屏首页业务中，调整小屏幕时文字大小、竖线高度）
+  media: {
+    type: Boolean,
+    default: false,
+  },
+  // 分割线
   divider: {
     type: Boolean,
     default: false,
