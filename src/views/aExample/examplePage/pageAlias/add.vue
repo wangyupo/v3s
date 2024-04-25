9ff79fd10f60fc869d8efe0f53e1c65d173a085d
wangyupo <1106774262@qq.com>
2024-04-25 15:31:05 +0800

    [^]优化页面别名示例页

--- src/views/aExample/examplePage/pageAlias/add.vue
+++ src/views/aExample/examplePage/pageAlias/add.vue
@@ -1,13 +1,39 @@
 <template>
   <!-- 添加页面 -->
   <div>
-    <p>
+    <p class="flex items-center">
       添加页面
-      <span class="italic font-bold">（注意看面包屑导航名称的变化）</span>
+      <span class="flex items-center italic font-bold">
+        （注意看面包屑导航名称的变化
+        <el-icon class="arrow ml-1" size="20"><Top /></el-icon>
+        ）
+      </span>
     </p>
+    <p>也可以刷新页面，别名是可以留存的。</p>
   </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.arrow {
+  animation: arrow 1.6s infinite linear;
+}
+@keyframes arrow {
+  0% {
+    transform: translateY(0);
+  }
+  25% {
+    transform: translateY(-5px);
+  }
+  50% {
+    transform: translateY(0);
+  }
+  75% {
+    transform: translateY(5px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+</style>
