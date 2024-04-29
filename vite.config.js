82e7fa2b6295d3dbaebbca590454ddca4dec36b2
wangyupo <1106774262@qq.com>
2024-04-29 16:51:06 +0800

    [^]区分不同环境打包命令

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { visualizer } from "rollup-plugin-visualizer";
@@ -6,49 +6,53 @@ import { visualizer } from "rollup-plugin-visualizer";
 const versionData = require("./version.json"); // 引入版本号
 const PROXY_TARGET = ""; // 后端服务地址
 
-export default defineConfig({
-  base: "/v3s-admin/",
-  plugins: [
-    vue(),
-    visualizer({
-      gzipSize: true,
-      brotliSize: true,
-      emitFile: false,
-      filename: "test.html", // 分析图生成的文件名
-      open: true, // 如果存在本地服务端口，将在打包后自动展示
-    }),
-  ],
-  server: {
-    proxy: {
-      "/api": {
-        target: PROXY_TARGET,
-        changeOrigin: true,
-        configure: (proxy, options) => {
-          proxy.on("proxyRes", (proxyRes, req, res) => {
-            proxyRes.headers["x-proxy-url"] = PROXY_TARGET + req.originalUrl;
-          });
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()); // 获取当前环境下的变量
+
+  return {
+    base: env.VITE_BASE,
+    plugins: [
+      vue(),
+      visualizer({
+        gzipSize: true,
+        brotliSize: true,
+        emitFile: false,
+        filename: "test.html", // 分析图生成的文件名
+        open: true, // 如果存在本地服务端口，将在打包后自动展示
+      }),
+    ],
+    server: {
+      proxy: {
+        "/api": {
+          target: PROXY_TARGET,
+          changeOrigin: true,
+          configure: (proxy, options) => {
+            proxy.on("proxyRes", (proxyRes, req, res) => {
+              proxyRes.headers["x-proxy-url"] = PROXY_TARGET + req.originalUrl;
+            });
+          },
         },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].[hash].v${versionData.version}.js`,
-        chunkFileNames: `assets/[name].[hash].v${versionData.version}.js`,
-        assetFileNames: `assets/[name].[hash].v${versionData.version}.[ext]`,
-        manualChunks: {
-          echarts: ["echarts"],
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name].[hash].v${versionData.version}.js`,
+          chunkFileNames: `assets/[name].[hash].v${versionData.version}.js`,
+          assetFileNames: `assets/[name].[hash].v${versionData.version}.[ext]`,
+          manualChunks: {
+            echarts: ["echarts"],
+          },
         },
       },
     },
-  },
-  define: {
-    __APP_VERSION__: JSON.stringify(versionData.version), // 版本号全局常量
-  },
+    define: {
+      __APP_VERSION__: JSON.stringify(versionData.version), // 版本号全局常量
+    },
+  };
 });
